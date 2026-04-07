e637b15eb604e1c335509957ea8876743e1b6395
Dardeos <87314938+Dardeos@users.noreply.github.com>
2026-04-07 21:50:35 +0200

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,61 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Event, Participant, Registration
+from .serializers import EventSerializer, ParticipantSerializer, RegistrationSerializer,CustomTokenObtainPairSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.exceptions import PermissionDenied
+
+def is_admin(user):
+    return user.is_staff
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    filter_backends = [DjangoFilterBackend]
+    def get_queryset(self):
+        qs = Event.objects.all()
+        status = self.request.query_params.get('status')
+        date = self.request.query_params.get('date')
+
+        if status:
+            qs = qs.filter(status=status)
+        if date:
+            qs = qs.filter(date__date=date)
+
+        return qs
+
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated()]
+        return [IsAuthenticated()] if is_admin(self.request.user) else [IsAuthenticated()]
+
+    def check_permissions(self, request):
+        super().check_permissions(request)
+        if self.action not in ['list', 'retrieve'] and not is_admin(request.user):
+            raise PermissionDenied("Vous n'avez pas les droits pour effectuer cette action.")
+
+class ParticipantViewSet(viewsets.ModelViewSet):
+    queryset = Participant.objects.all()
+    serializer_class = ParticipantSerializer
+
+    def check_permissions(self, request):
+        super().check_permissions(request)
+        if self.action not in ['list', 'retrieve'] and not is_admin(request.user):
+            from rest_framework.exceptions import PermissionDenied
+            raise PermissionDenied("Vous n'avez pas les droits pour effectuer cette action.")
+
+class RegistrationViewSet(viewsets.ModelViewSet):
+    queryset = Registration.objects.all()
+    serializer_class = RegistrationSerializer
+
+    def check_permissions(self, request):
+        super().check_permissions(request)
+        # FIX: Added 'create' to the allowed actions list
+        if self.action not in ['list', 'retrieve', 'create'] and not is_admin(request.user):
+            from rest_framework.exceptions import PermissionDenied
+            raise PermissionDenied("Vous n'avez pas les droits pour effectuer cette action.")
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
